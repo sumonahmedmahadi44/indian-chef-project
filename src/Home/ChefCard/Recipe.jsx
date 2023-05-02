6630a57a509fd2741c216b4af6fecbc69cad9fe8
sumonahmedmahadi44 <dmxsumon2@gmail.com>
2023-05-02 19:33:33 +0600

    added recipe card

--- src/Home/ChefCard/Recipe.jsx
+++ src/Home/ChefCard/Recipe.jsx
@@ -1,23 +1,23 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-// const Recipe = ({recipe}) => {
-//     const {name , rating , ingredients, image_food,method,} = recipe;
-//     return (
+const Recipe = ({recipe}) => {
+    const {name , rating , ingredients, image_food,method,} = recipe;
+    return (
     
-//             <div className="card w-96 bg-base-100 shadow-xl image-full h-100 ">
-//   <figure><img src={image_food} alt="Shoes" /></figure>
-//   <div className="card-body">
-//     <h2 className="card-title font-extrabold text-xl text-sky-400">{name}</h2>
-//     <p><span className='font-bold text-yellow-200 text-xl'>Ingredients of this Recipe :</span> {ingredients}</p>
-//     <p><span className='font-bold text-yellow-200 text-xl'>Method of this Recipe :</span> {method}</p>
-//     <p><span className='font-bold text-yellow-200 text-xl'>Ratings : </span>{rating}</p>
+            <div className="card w-96 bg-base-100 shadow-xl image-full h-100 ">
+  <figure><img src={image_food} alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title font-extrabold text-xl text-sky-400">{name}</h2>
+    <p><span className='font-bold text-yellow-200 text-xl'>Ingredients of this Recipe :</span> {ingredients}</p>
+    <p><span className='font-bold text-yellow-200 text-xl'>Method of this Recipe :</span> {method}</p>
+    <p><span className='font-bold text-yellow-200 text-xl'>Ratings : </span>{rating}</p>
 
-//   </div>
-// </div>
+  </div>
+</div>
             
        
-//     );
-// };
+    );
+};
 
-// export default Recipe;
+export default Recipe;
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,7 +1,9 @@
 import React, { createContext, useState } from 'react';
-
+import { getAuth } from "firebase/auth";
+import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
+const Auth = getAuth(app)
 const AuthProvider = ({children}) => {
     const [user,setUser] =useState({})
     return (
