0e4903a3db42b5ffe501c3e2e56a76808f0dd8b1
sumonahmedmahadi44 <dmxsumon2@gmail.com>
2023-05-02 19:13:14 +0600

    added card

--- src/Home/ChefCard/ChefCard.jsx
+++ src/Home/ChefCard/ChefCard.jsx
@@ -1,22 +1,22 @@
-// import { data } from 'autoprefixer';
-// import React, { useEffect, useState } from 'react';
-// import { useLoaderData } from 'react-router-dom';
-// import ChefCardView from './ChefCardView';
+import { data } from 'autoprefixer';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import ChefCardView from './ChefCardView';
 
-// const ChefCard = () => {
-//    const [chefs,setChefs] = useState([]);
+const ChefCard = () => {
+   const [chefs,setChefs] = useState([]);
    
 
-//    useEffect(() =>{
-//     fetch('http://localhost:5000/chefs').then(res => res.json()).then(data => setChefs(data)).catch(error => console.error(error))
-//    },[])
-//     return (
-//         <div className='grid md:grid-cols-3 gap-10 '>
-//             {
-//                 chefs.map(chef =><ChefCardView key={chef.id} chef = {chef}></ChefCardView> )
-//             }
-//         </div>
-//     );
-// };
+   useEffect(() =>{
+    fetch('http://localhost:5000/chefs').then(res => res.json()).then(data => setChefs(data)).catch(error => console.error(error))
+   },[])
+    return (
+        <div className='grid md:grid-cols-3 gap-10 '>
+            {
+                chefs.map(chef =><ChefCardView key={chef.id} chef = {chef}></ChefCardView> )
+            }
+        </div>
+    );
+};
 
-// export default ChefCard;
+export default ChefCard;
